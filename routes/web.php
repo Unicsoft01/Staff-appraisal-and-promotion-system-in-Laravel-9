b3ad901cd96fa867adce82fa4047a992223c2752
Yakub Muhammed <yakubmuhammed51@gmail.com>
2022-06-29 01:00:46 -0700

    Admin panel setup part 1
    the structure of the admin panel has been initialised but without css stylings

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 //     Route::get('/dashboard', 'Index')->middleware(['auth'])->name('dashboard'); 
 // });
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth'])->name('dashboard');
 
 
