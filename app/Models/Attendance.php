26c10db7e26f76fc46e03fb0c97dc63b18746a49
Yakub Muhammed <yakubmuhammed51@gmail.com>
2022-08-19 00:52:04 +0100

    model can be used to modify table contents

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
-    // relationship between attendance and users can be set her3
+    // relationship between attendance and users can be set here
+
+    // i can state what i want to change the defualt values here
+    // eg table name, default id value, show/hide timestamp
+    // protected $table = 'Attendance';
+    // protected $primaryKey = 'item_id';
+    // protected $timestamps = 'false';
+
 }
