3302596c05ea45d2765d406a30fdefb66aeb47b3
Yakub Muhammed <yakubmuhammed51@gmail.com>
2022-08-18 01:08:10 +0100

    configure form to take file and form fields

--- resources/views/admin/edit_profile.blade.php
+++ resources/views/admin/edit_profile.blade.php
@@ -5,35 +5,33 @@
   <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
       <div class="card-body">
-        <div class="row">
-          
-        <div class="col-8 col-md-10 mt-3">
-          <p class="card-title">{{Auth::user()->username}}'s Profile Page</p>
-        </div>
-        <div class="col-4 col-md-2 mb-2">
-          <button class="btn btn-success">
-            Update
-          </button>
-        </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-          <center>
-              <img src="{{asset('/backend_asset/images/faces/face28.jpg')}}" alt="profile"/>
-              <input type="file" class="bg-info-inverse" placeholder="Username" name="">
-          </center>
-
-                <div class="card-body">
-                    <h4 class="card-title">Name: <input type="text" value="{{Auth::user()->name}}" name=""></h4>
-                    <hr>
-                    <h4 class="card-title">Email: <input type="text" value="{{Auth::user()->email}}" name=""></h4>
-                    <hr>
-                    <h4 class="card-title">Username: <input type="text" value="{{Auth::user()->username}}" name=""></h4>
-                    <hr>
+          <form method="POST" action="{{ route('store.profile') }}" enctype="multipart/form-data">
+            <div class="row">
+              <div class="col-8 col-md-10 mt-3">
+                <p class="card-title">Edit {{Auth::user()->username}}'s Profile</p>
+              </div>
+                <div class="col-4 col-md-2 mb-2">
+                  <input type="submit" value="Update" class="btn btn-success">
                 </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                  <center>
+                    <img src="{{asset('/backend_asset/images/faces/face28.jpg')}}" alt="profile"/>
+                    <input type="file" class="bg-info-inverse" placeholder="Username" name="profile_image">
+                  </center>
 
-        </div>
-      </div>
+                    <div class="card-body">
+                        <h4 class="card-title">Name: <input type="text" value="{{Auth::user()->name}}" name="name"></h4>
+                        <hr>
+                        <h4 class="card-title">Email: <input type="text" value="{{Auth::user()->email}}" name="email"></h4>
+                        <hr>
+                        <h4 class="card-title">Username: <input type="text" value="{{Auth::user()->username}}" name="username"></h4>
+                        <hr>
+                    </div>
+              </div>
+            </div>
+          </form>
       </div>
     </div>
   </div>
